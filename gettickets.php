d7d76692c8a01c727ed25f15b4e5a51dfcf7c598
Chad <euhedralforms@protonmail.com>
2021-03-22 21:58:12 -0500

    more connect attempts

--- gettickets.php
+++ gettickets.php
@@ -6,8 +6,15 @@
 
     $data = array();
 
-    $conn = new mysqli($servername, $username, $password, $dbname, 1433); 
-    if ($conn->connect_errno){
+    //$conn = new mysqli($servername, $username, $password, $dbname, 1433); 
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306)
+    // if ($conn->connect_errno){
+    //     echo '<h3> Connect failed! </h3>'
+    //     die("Connection failed: " . $conn->connect_error);
+    // }
+    
+    if (mysqli_connect_errno($conn)){
         echo '<h3> Connect failed! </h3>'
         die("Connection failed: " . $conn->connect_error);
     }   
