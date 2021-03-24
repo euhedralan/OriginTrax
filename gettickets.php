5688dfb09b118c1ca668a0eca16ee9371a8655b1
Chad <euhedralforms@protonmail.com>
2021-03-24 12:00:40 -0500

    123123

--- gettickets.php
+++ gettickets.php
@@ -6,24 +6,6 @@
 
     $data = array();
 
-    // $conn = new mysqli($servername, $username, $password, $dbname, 1433);    
-    // if ($conn->connect_errno){
-    //    echo '<h3> Connect failed! </h3>'
-    //    die("Connection failed: " . $conn->connect_error);
-    // }
-
-    //Initializes MySQLi
-    // $conn = mysqli_init();
-    
-    // mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);    
-    // // Establish the connection
-    // mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
-    
-    // //If connection failed, show the error
-    // if (mysqli_connect_errno($conn))
-    // {
-    //     die('Failed to connect to MySQL: '.mysqli_connect_error());
-    // }   
     // PHP Data Objects(PDO) Sample Code:
     try {
         $conn = new PDO("sqlsrv:server = tcp:origintrax1.database.windows.net,1433; Database = origintrax", "playerone", "SGRE@123");
@@ -35,15 +17,15 @@
     }     
      
     $sql = "SELECT * FROM tickets";
-    //$result = $conn->query($sql);
+    $result = $conn->query($sql);
     
-    foreach($conn->query($sql) as $row){
-        $data[] = $row;
+    //foreach($conn->query($sql) as $row){
+    //    $data[] = $row;
+    //}
+    
+    while($row = $result->fetch(PDO::FETCH_ASSOC))  {
+         $data[] = $row;
     }
-    
-    // while($row = $result->fetch_array(MYSQLI_ASSOC))  {
-    //     $data[] = $row;
-    // }
 
     echo json_encode($data);    
 ?>
