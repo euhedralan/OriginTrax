1816377f8055a36b63dd4dffeb7e04435505ca8b
Chad <euhedralforms@protonmail.com>
2021-03-23 21:51:44 -0500

    new

--- gettickets.php
+++ gettickets.php
@@ -6,11 +6,25 @@
 
     $data = array();
 
-    $conn = new mysqli($servername, $username, $password, $dbname, 1433);    
-    if ($conn->connect_errno){
-       echo '<h3> Connect failed! </h3>'
-       die("Connection failed: " . $conn->connect_error);
-    }
+    // $conn = new mysqli($servername, $username, $password, $dbname, 1433);    
+    // if ($conn->connect_errno){
+    //    echo '<h3> Connect failed! </h3>'
+    //    die("Connection failed: " . $conn->connect_error);
+    // }
+
+    //Initializes MySQLi
+    $conn = mysqli_init();
+    
+    mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+    
+    // Establish the connection
+    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+    
+    //If connection failed, show the error
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }    
      
     $sql = "SELECT * FROM tickets";
     $result = $conn->query($sql);
--- searchtickets.php
+++ searchtickets.php
@@ -4,12 +4,24 @@
     $password="SGRE@123";
     $dbname="origintrax";
 
-    $conn = new mysqli($servername, $username, $password, $dbname, 1433); 
-    if ($conn->connect_errno){
-        echo '<h3> Connect failed! </h3>'
-        die("Connection failed: " . $conn->connect_error);
-    }   
+    // $conn = new mysqli($servername, $username, $password, $dbname, 1433); 
+    // if ($conn->connect_errno){
+    //     echo '<h3> Connect failed! </h3>'
+    //     die("Connection failed: " . $conn->connect_error);
+    // }   
+    //Initializes MySQLi
+    $conn = mysqli_init();
     
+    mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+    
+    // Establish the connection
+    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+    
+    //If connection failed, show the error
+    if (mysqli_connect_errno($conn))
+    {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }    
     $id = mysqli_real_escape_string($conn, $_POST['id']);
 
     $sql = "SELECT * FROM tickets WHERE ticketNumber='". $id. "'";
